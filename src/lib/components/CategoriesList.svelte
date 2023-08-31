4d9db72b2bd44f5e2ebaa5c51e9af7a31eac0c1c
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2023-08-31 23:58:48 +0200

    add <CategoriesList />

---
+++ src/lib/components/CategoriesList.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import type { Category } from '$lib/interfaces/Category';
+	import CategoryCard from './CategoryCard.svelte';
+
+	export let categories: Category[];
+</script>
+
+<div class="categories-list">
+	{#each categories as category}
+		<CategoryCard {category} />
+	{/each}
+</div>
+
+<style>
+	.categories-list {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 3em; /* Adjust the gap between cards as needed */
+		max-width: 60%;
+		margin: auto;
+	}
+
+	@media only screen and (max-width: 992px) {
+		.categories-list {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			gap: 2em; /* Adjust the gap between cards as needed */
+			max-width: 75%;
+			margin: auto;
+		}
+	}
+
+	@media only screen and (max-width: 768px) {
+		.categories-list {
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			gap: 1em; /* Adjust the gap between cards as needed */
+			max-width: 85%;
+			margin: auto;
+		}
+	}
+
+	@media only screen and (max-width: 576px) {
+		.categories-list {
+			display: grid;
+			grid-template-columns: repeat(1, 1fr);
+			max-width: 100%;
+		}
+	}
+</style>
--- src/routes/sklep/+page.svelte
+++ src/routes/sklep/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import CategoryCard from '$lib/components/CategoryCard.svelte';
+	import CategoriesList from '$lib/components/CategoriesList.svelte';
 	import type { Category } from '$lib/interfaces/Category';
 
 	const categories: Category[] = [
@@ -22,48 +22,4 @@
 
 <hr />
 
-<div class="categories-list">
-	{#each categories as category}
-		<CategoryCard {category} />
-	{/each}
-</div>
-
-<slot />
-
-<style>
-	.categories-list {
-		display: grid;
-		grid-template-columns: repeat(2, 1fr);
-		gap: 3em; /* Adjust the gap between cards as needed */
-		max-width: 60%;
-		margin: auto;
-	}
-
-	@media only screen and (max-width: 992px) {
-		.categories-list {
-			display: grid;
-			grid-template-columns: repeat(2, 1fr);
-			gap: 2em; /* Adjust the gap between cards as needed */
-			max-width: 75%;
-			margin: auto;
-		}
-	}
-
-	@media only screen and (max-width: 768px) {
-		.categories-list {
-			display: grid;
-			grid-template-columns: repeat(2, 1fr);
-			gap: 1em; /* Adjust the gap between cards as needed */
-			max-width: 85%;
-			margin: auto;
-		}
-	}
-
-	@media only screen and (max-width: 576px) {
-		.categories-list {
-			display: grid;
-			grid-template-columns: repeat(1, 1fr);
-			max-width: 100%;
-		}
-	}
-</style>
+<CategoriesList {categories} />
