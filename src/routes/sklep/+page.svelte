3d48a956f8c8e3e094401c5fb7004f579014a307
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2023-09-03 22:55:15 +0200

    add <ProductsListWithFilter /> to /swieczniki/[subcategory] & /archiwum

--- src/routes/sklep/+page.svelte
+++ src/routes/sklep/+page.svelte
@@ -2,6 +2,13 @@
 	import CategoriesList from '$lib/components/CategoriesList.svelte';
 	import type { Category } from '$lib/interfaces/Category';
 
+	export let data;
+
+	const archivedProducts = data.products.filter((p) => !p.active);
+	const archivedProductImageSrc = archivedProducts.length
+		? archivedProducts[0].images[0]
+		: '/candle.jpeg';
+
 	const categories: Category[] = [
 		{
 			imgSrc: '/candleholder.jpeg',
@@ -16,7 +23,7 @@
 			title: 'Świece'
 		},
 		{
-			imgSrc: '/candle.jpeg',
+			imgSrc: archivedProductImageSrc,
 			name: 'archiwum',
 			slug: 'archiwum',
 			title: 'Archiwum (dostępne na zamówienie)'
